9039bb52f9f255bf254cb7657feb62457a1f8863
yduchesne <yanickduchesne@gmail.com>
2022-04-16 17:36:42 -0700

    Removed use of curl in AccessToken function

--- scripts/AdHelper.ps1
+++ scripts/AdHelper.ps1
@@ -9,7 +9,9 @@ function AuthADConnect($username, $nonSecurePass) {
     Connect-AzureAD -Credential AuthCreds($username, $nonSecurePass)
 }
 function AccessToken($identityEnpoint, $identityHeader) {
-    curl "$identityEnpoint?resource=https://management.azure.com&api-version=2017-09-01" -H @{"secret" = $identityHeader}
+    $headers=@{"secret"=$identityHeader}
+    $response = Invoke-WebRequest -UseBasicParsing -Uri "$identityEnpoint?resource=https://storage.azure.com/&api-version=2017-09-01" -Headers $headers
+    $response.RawContent
 }
 
 # ============================================================================
